35ab07b68e30c5ee5001ba5e1346bbcc8b28054f
Corbitt Arfken <caarfken@protonmail.com>
2021-11-29 11:50:13 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ wget https://github.com/caarfken/corscript/archive/refs/heads/main.zip
 echo completed
 echo If script fails here, install unzip
 unzip main.zip -d ~/.corscript/bin
-echo alias corscript='python3 ~/.corscript/bin'>> ~/.bashrc
+echo alias corscript="python3 ~/.corscript/bin/corscript-main/corscript.py" >> ~/.bashrc
