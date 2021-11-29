c6647b0f23186ff6a2c4d50959dff11d9c8c4e12
Corbitt Arfken <caarfken@protonmail.com>
2021-11-29 11:30:44 -0400

    bugfix

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-mkdir ~/.corscript/lib
-mkdir ~/.corscript/bin
+mkdir -p ~/.corscript/lib
+mkdir -p ~/.corscript/bin
 echo downloading...
 wget https://github.com/caarfken/corscript/archive/refs/heads/main.zip
 echo completed
 echo If script fails here, install unzip
 unzip main.zip -d ~/.corscript/bin
-echo alias corscript='python3 ~/.corscript/bin'>> .bashrc
+echo alias corscript='python3 ~/.corscript/bin'>> ~/.bashrc
