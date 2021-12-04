470af9bf1a49ef934c44ba33bc70a7ae31962260
Corbitt Arfken <caarfken@protonmail.com>
2021-12-04 16:15:02 -0400

    add corpkg

--- install.sh
+++ install.sh
@@ -3,8 +3,10 @@ mkdir -p ~/.corscript/lib
 mkdir -p ~/.corscript/bin
 echo downloading...
 wget https://github.com/caarfken/corscript/archive/refs/heads/main.zip
+wget https://raw.githubusercontent.com/corscript/corscript-install/main/corpkg.sh
 echo completed
 echo If script fails here, install unzip
 unzip main.zip -d ~/.corscript/bin
 echo alias corscript=\"python3 ~/.corscript/bin/corscript-main/corscript.py\" >> ~/.bashrc
+echo alias corpkg=\"~/.corscript/bin/corpkg.sh\" >> ~/.bashrc
 rm main.zip
--- update.sh
+++ update.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo downloading update...
 wget https://github.com/caarfken/corscript/archive/refs/heads/main.zip
+wget https://raw.githubusercontent.com/corscript/corscript-install/main/corpkg.sh
 echo completed
 unzip main.zip -d ~/.corscript/bin
+mv cpack.sh ~/.corscript/bin
 rm main.zip
