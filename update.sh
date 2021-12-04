476039afa7f3252e993da45e4f07c3e38d10158c
Corbitt Arfken <caarfken@protonmail.com>
2021-12-04 16:29:19 -0400

    fix critical bug

--- update.sh
+++ update.sh
@@ -4,5 +4,5 @@ wget https://github.com/caarfken/corscript/archive/refs/heads/main.zip
 wget https://raw.githubusercontent.com/corscript/corscript-install/main/corpkg.sh
 echo completed
 unzip main.zip -d ~/.corscript/bin
-mv cpack.sh ~/.corscript/bin
+mv corpkg.sh ~/.corscript/bin
 rm main.zip
